ed45bfd6fc7ea87947a83e86c7c5c7fa89a231ff
Christian Hahn <17895694+Chris1415@users.noreply.github.com>
2025-07-30 15:28:29 +0200

    Readded PartialDesignDynamicPlaceholder.tsx to the basic-nextjs head app (#69)

--- examples/basic-nextjs/.sitecore/component-map.ts
+++ examples/basic-nextjs/.sitecore/component-map.ts
@@ -3,12 +3,16 @@ import { BYOCWrapper, NextjsContentSdkComponent, FEaaSWrapper } from '@sitecore-
 import { Form } from '@sitecore-content-sdk/nextjs';
 // end of built-in components
 
+// Components imported from the app itself
+import * as PartialDesignDynamicPlaceholder from 'src/components/partial-design-dynamic-placeholder/PartialDesignDynamicPlaceholder';
+
 
 // Components must be registered within the map to match the string key with component name in Sitecore
 export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['BYOCWrapper', BYOCWrapper],
   ['FEaaSWrapper', FEaaSWrapper],
   ['Form', Form],
+  ['PartialDesignDynamicPlaceholder', PartialDesignDynamicPlaceholder],
 ]);
 
 export default componentMap;
